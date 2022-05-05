929551eccf01c74698f32905c693ff41eb723e7e
Batuhan ARSLAN <arslanbatu97@gmail.com>
2022-05-05 23:07:52 +0300

    pointers examples

---
+++ calculator.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main(){
+
+    printf("*****WELCOME THE BASIC CALCULATOR*****");
+
+    int a;
+    int b;
+
+    printf("\nPlease Enter First number:");
+    scanf("%d",&a);
+    printf("Please Enter Second number:");
+    scanf("%d",&b);
+
+    printf("\nSum is :%d",a+b);
+    printf("\nExtraction is :%d",a-b);
+    printf("\nMultipilication is :%d",a*b);
+    printf("\nDivision is :%f",a/b);
+
+
+
+}
+
+
