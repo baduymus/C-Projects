25455956c40c93ef6181e29e683f1eccb9d4a3c9
Batuhan ARSLAN <arslanbatu97@gmail.com>
2022-05-05 18:57:31 +0300

    square a*a

---
+++ square_example.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+
+//In this project i  calculate the area of the square with getting user inputs
+
+int main(){
+
+    printf("**********WELCOME SQUARE FIELD CALCULATER**********");
+
+    
+    printf("\nPlease enter an input for calculating square field :");
+    int a;
+    scanf("%d",&a);
+
+    printf("\nThe square field is : %d meter square",a*a);
+
+    
+
+}
+
+
